da08d69e4fefd3d545531841bd5c6895a0cfa838
Eashan Kaushik <eashank@amazon.com>
2024-02-25 14:50:13 -0500

    cleanm

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Button, ButtonGroup } from '@chakra-ui/react'
-function App() {
-  return (
-    <Button colorScheme='blue'>Button</Button>
-  )
-}
-
-export default App
